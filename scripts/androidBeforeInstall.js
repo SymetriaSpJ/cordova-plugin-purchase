8080f34a038b65f7c6928bcbfc95072f84c70a6a
gczudowski <gczudowski@gmail.com>
2021-03-31 10:32:51 +0200

    wip

--- scripts/androidBeforeInstall.js
+++ scripts/androidBeforeInstall.js
@@ -24,45 +24,45 @@ module.exports = function (ctx) {
   rmByPathSync(path.join(platformRoot, 'app', 'src', 'main', 'aidl', 'com', 'android', 'vending', 'billing', 'IInAppBillingService.aidl'));
   rmByPathSync(path.join(platformRoot, 'src', 'com', 'android', 'vending', 'billing', 'IInAppBillingService.aidl'));
 
-  // Android Studio or similar
-  var baseDir = '';
-  if (!fs.existsSync(path.join(platformRoot, 'res'))) {
-    baseDir = path.join('app', 'src', 'main');
-	}
-
-  var xmlDir = path.join(platformRoot, baseDir, resDir);
-  mkdirByPathSync(xmlDir);
-
-  var xmlFile = path.join(xmlDir, 'billing_key_param.xml');
-  fs.writeFileSync(xmlFile, xmlContent);
-
-	// Source: https://stackoverflow.com/questions/31645738/how-to-create-full-path-with-nodes-fs-mkdirsync
-	function mkdirByPathSync(targetDir) {
-		var initDir = path.isAbsolute(targetDir) ? path.sep : '';
-
-		return targetDir.split(path.sep).reduce(function (parentDir, childDir) {
-			var curDir = path.resolve(parentDir, childDir);
-			try {
-				fs.mkdirSync(curDir);
-			} catch (err) {
-				if (err.code === 'EEXIST') { // curDir already exists!
-					return curDir;
-				}
-
-				// To avoid `EISDIR` error on Mac and `EACCES`-->`ENOENT` and `EPERM` on Windows.
-				if (err.code === 'ENOENT') { // Throw the original parentDir error on curDir `ENOENT` failure.
-					throw new Error(`EACCES: permission denied, mkdir '${parentDir}'`);
-				}
-
-				var caughtErr = ['EACCES', 'EPERM', 'EISDIR'].indexOf(err.code) > -1;
-				if (!caughtErr || caughtErr && targetDir === curDir) {
-					throw err; // Throw if it's just the last created dir.
-				}
-			}
-
-			return curDir;
-		}, initDir);
-	}
+  // // Android Studio or similar
+  // var baseDir = '';
+  // if (!fs.existsSync(path.join(platformRoot, 'res'))) {
+  //   baseDir = path.join('app', 'src', 'main');
+	// }
+  //
+  // var xmlDir = path.join(platformRoot, baseDir, resDir);
+  // mkdirByPathSync(xmlDir);
+  //
+  // var xmlFile = path.join(xmlDir, 'billing_key_param.xml');
+  // fs.writeFileSync(xmlFile, xmlContent);
+  //
+	// // Source: https://stackoverflow.com/questions/31645738/how-to-create-full-path-with-nodes-fs-mkdirsync
+	// function mkdirByPathSync(targetDir) {
+	// 	var initDir = path.isAbsolute(targetDir) ? path.sep : '';
+  //
+	// 	return targetDir.split(path.sep).reduce(function (parentDir, childDir) {
+	// 		var curDir = path.resolve(parentDir, childDir);
+	// 		try {
+	// 			fs.mkdirSync(curDir);
+	// 		} catch (err) {
+	// 			if (err.code === 'EEXIST') { // curDir already exists!
+	// 				return curDir;
+	// 			}
+  //
+	// 			// To avoid `EISDIR` error on Mac and `EACCES`-->`ENOENT` and `EPERM` on Windows.
+	// 			if (err.code === 'ENOENT') { // Throw the original parentDir error on curDir `ENOENT` failure.
+	// 				throw new Error(`EACCES: permission denied, mkdir '${parentDir}'`);
+	// 			}
+  //
+	// 			var caughtErr = ['EACCES', 'EPERM', 'EISDIR'].indexOf(err.code) > -1;
+	// 			if (!caughtErr || caughtErr && targetDir === curDir) {
+	// 				throw err; // Throw if it's just the last created dir.
+	// 			}
+	// 		}
+  //
+	// 		return curDir;
+	// 	}, initDir);
+	// }
 
   function rmByPathSync(targetFile) {
     if (fs.existsSync(targetFile))
